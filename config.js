e59323868fd155ed7f598a068bfa52c99091f730
mohdayazbhati <mohdayazbhati88@gmail.com>
2024-03-01 17:43:53 +0530

    sd

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+      'mongodb+srv://mohdayazbhati:Ayaz_200300@books-store-mern.21oayay.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
